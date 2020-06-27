7a23fbac2a91ab3297c097724685480afd57ee84
n1tranquilla <n1tranquilla@gmail.com>
2020-06-27 08:25:08 -0300

    Create mqtt_subscriber_push_notification_to_channel.py

---
+++ python/mqtt_subscriber_push_notification_to_channel.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import paho.mqtt.client as mqtt
+import requests
+import sys
+from pushbullet import Pushbullet
+
+pb = Pushbullet(sys.argv[1])
+channel_name = sys.argv[2]
+
+channel = pb.get_channel(channel_name)
+
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code "+str(rc))
+  client.subscribe("torrent/#")
+
+def on_message(client, userdata, msg):
+  file = msg.payload.decode()
+  result = channel.push_note('Torrent complete', '{} has finished downlaoding'.format(file))
+  print('{} has finished downloading'.format(file))
+
+client = mqtt.Client()
+client.connect("localhost",1883,60)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.loop_forever()
