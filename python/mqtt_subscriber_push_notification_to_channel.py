d071f52651818765400d1642282f0f6d83f113b0
n1tranquilla <n1tranquilla@gmail.com>
2020-06-27 08:33:33 -0300

    Update mqtt_subscriber_push_notification_to_channel.py

--- python/mqtt_subscriber_push_notification_to_channel.py
+++ python/mqtt_subscriber_push_notification_to_channel.py
@@ -7,12 +7,13 @@ from pushbullet import Pushbullet
 
 pb = Pushbullet(sys.argv[1])
 channel_name = sys.argv[2]
+mqtt_channel = sys.argv[3]
 
 channel = pb.get_channel(channel_name)
 
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code "+str(rc))
-  client.subscribe("torrent/#")
+  client.subscribe(mqtt_channel)
 
 def on_message(client, userdata, msg):
   file = msg.payload.decode()
